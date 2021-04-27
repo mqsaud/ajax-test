e4cad6bc4df307ace0160476e9a18a4328338471
mqsaud <mqsaud@gmail.com>
2021-04-27 18:52:05 +0000

    Modify and add write to page function

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
-function getData(cb){
-var xhr = new XMLHttpRequest();
+const baseURL = "https://ci-swapi.herokuapp.com/api/";
 
-xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
-xhr.send();
+function getData(type, cb) {
+    var xhr = new XMLHttpRequest();
 
-xhr.onreadystatechange = function(){
-  if (this.readyState == 4 && this.status == 200) {
-        cb(JSON.parse(this.responseText));
-   };
- };
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
+
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
 }
-getData(function(data){
-    console.log(data);
-});
+
+function writeToDocument(type) {
+    getData(type, function(data) {
+        document.getElementById("data").innerHTML = data;
+    });
+}

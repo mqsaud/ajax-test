fdd828d50b65b6d74decddbd0af44f96e3025f50
mqsaud <mqsaud@gmail.com>
2021-04-27 15:03:56 +0000

    Add json.parse() function

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 var xhr = new XMLHttpRequest();
+var data;
 
 xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
 
 xhr.send();
 
+function setData(jsonData){
+    data = jsonData;
+    console.log(data);
+};
+
 xhr.onreadystatechange = function(){
   if (this.readyState == 4 && this.status == 200) {
-    document.getElementById("data").innerHTML = this.responseText;
+        setData(JSON.parse(this.responseText));
+    
   };
 };

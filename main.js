590f087aa30d4891fb72f6c5e7b8f4f451d8a2f5
mqsaud <mqsaud@gmail.com>
2021-04-27 18:01:13 +0000

    Improve the code with callback function

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
+function getData(cb){
 var xhr = new XMLHttpRequest();
-var data;
 
 xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
-
 xhr.send();
-
-function setData(jsonData){
-    data = jsonData;
-    console.log(data);
-};
 
 xhr.onreadystatechange = function(){
   if (this.readyState == 4 && this.status == 200) {
-        setData(JSON.parse(this.responseText));
-    
-  };
-};
+        cb(JSON.parse(this.responseText));
+   };
+ };
+}
+getData(function(data){
+    console.log(data);
+});
